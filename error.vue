781feeff03d885f02fced8c8a75191359febc2e2
bernadlein <108117547+bernadlein@users.noreply.github.com>
2025-10-02 20:10:54 +0700

    Create error.vue

---
+++ error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{ error: any }>()
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+  <section class="section">
+    <div class="max-w-xl mx-auto text-center">
+      <h1 class="text-3xl font-extrabold">Oops!</h1>
+      <p class="mt-2 text-slate-600">Something went wrong.</p>
+      <p class="mt-2 text-xs text-slate-500">{{ error?.statusCode }} – {{ error?.message }}</p>
+      <div class="mt-6">
+        <ui-button @click="handleError">Back to Home</ui-button>
+      </div>
+    </div>
+  </section>
+</template>
