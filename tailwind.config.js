56a95c3164890a74eb313845d24f6ec8b3b47222
bernadlein <108117547+bernadlein@users.noreply.github.com>
2025-10-02 21:40:35 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
           800:'#166534',900:'#14532d'
         },
         limex: { 500:'#84cc16' },
-        amberx: { 500:'#f59e0b', 600:'#d97706' }
+        amberx: { 600:'#d97706' }
       },
       boxShadow: {
         soft: '0 1px 2px rgba(0,0,0,.04), 0 8px 20px rgba(22,163,74,.08)',
