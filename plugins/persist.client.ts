08df37595795a7a4dab85e032f14d556e4bc8595
bernadlein <108117547+bernadlein@users.noreply.github.com>
2025-10-03 02:34:54 +0700

    Update persist.client.ts

--- plugins/persist.client.ts
+++ plugins/persist.client.ts
@@ -1,21 +1,13 @@
-// plugins/persist.client.ts
 import { setLocale } from '#i18n'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const currency = useCurrency()
+export default defineNuxtPlugin(() => {
   if (!process.client) return
 
-  // Currency: load & persist
-  const savedC = localStorage.getItem('currency')
-  if (savedC === 'IDR' || savedC === 'USD') currency.value = savedC as any
-  watch(currency, (v) => localStorage.setItem('currency', v))
+  // currency
+  const c = localStorage.getItem('currency')
+  if (c === 'IDR' || c === 'USD') useCurrency().value = c as any
 
-  // Locale: load & persist (gunakan composer dari nuxt i18n)
-  nuxtApp.hook('app:mounted', () => {
-    const savedL = localStorage.getItem('locale')
-    if (savedL === 'id' || savedL === 'en') setLocale(savedL)
-
-    const composer: any = nuxtApp.$i18n
-    watch(composer.locale, (v: any) => localStorage.setItem('locale', String(v)))
-  })
+  // locale
+  const saved = localStorage.getItem('locale')
+  if (saved === 'id' || saved === 'en') setLocale(saved)
 })
