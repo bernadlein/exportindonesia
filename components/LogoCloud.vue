f50916e6a6ee172184ac7775cafe6930c7212c8f
bernadlein <108117547+bernadlein@users.noreply.github.com>
2025-10-02 12:14:41 +0700

    Create LogoCloud.vue

---
+++ components/LogoCloud.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="grid grid-cols-3 md:grid-cols-6 gap-6 opacity-70">
+    <img v-for="i in 6" :key="i" class="h-8 object-contain mx-auto"
+         :src="`https://placehold.co/160x32?text=Logo+${i}`" :alt="`Client ${i}`">
+  </div>
+</template>
