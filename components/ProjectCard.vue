d68fba8fe0390011c38b8b97058427a90b7f7c94
bernadlein <108117547+bernadlein@users.noreply.github.com>
2025-10-02 20:19:23 +0700

    Update ProjectCard.vue

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -3,7 +3,11 @@ defineProps<{ item: { img:string; title:string; desc:string } }>()
 </script>
 <template>
   <article class="card overflow-hidden">
-    <img :src="item.img" :alt="item.title" class="h-40 w-full object-cover">
+    <img 
+      :src="item.img" 
+      :alt="item.title" 
+      loading="lazy"
+      class="h-40 w-full object-cover">
     <div class="p-4">
       <h4 class="font-semibold">{{ item.title }}</h4>
       <p class="text-sm text-slate-600 mt-1">{{ item.desc }}</p>
